11eee7f0226f182aeb9be6d2868a30dcdb3576dc
Blaise Rivet <bls.rivet@gmail.com>
2014-03-28 00:33:39 +0000

    Update MotorEye.h file to fix compilation.

--- MotorEye.h
+++ MotorEye.h
@@ -13,8 +13,8 @@ class MotorEye
 		MotorEye(const int pin);
 
 		// returns if the duration is done
-		bool goGoogly(unsigned long currentTime_us, unsigned long duration_ms);
-		void on();
+		bool goGoogly(unsigned long currentTime_us, unsigned long googleOnTime_ms, unsigned long googleOffTime_ms, unsigned long googleVelocity, unsigned long totalGoogleTime);
+		void on(unsigned long motorVelocity);
 		void off();
 	private:
 		int _pin;
